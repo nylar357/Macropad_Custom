c5f5c1faaaf1d91aeb5f4974e124e0d7f14e7a3b
CͨoͦNᴛⷮRͬAͣ <37067686+nylar357@users.noreply.github.com>
2024-03-22 15:09:40 -0400

    Add files via upload

---
+++ macros/github.py
@@ -0,0 +1,27 @@
+from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
+
+app = {                      # REQUIRED dict, must be named 'app'
+    'name' : 'Github Shortcuts', # Application name
+        'order': 0,
+    'macros' : [             # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x004000, 'add', ['git add .\n']),
+        (0x004000, 'commit', ['git commit -m ']),
+        (0x004000, 'push', ['git push origin ']),
+        # 2nd row ----------
+        (0xff0076, 'pull', ['git pull origin ']),        
+        (0xff0076, 'main', ['main\n']),
+        (0xff0076, 'develop', ['develop\n']),
+        # 3rd row ----------
+        (0x0a0dd8, 'merge', ['git merge ']),
+        (0x0a0dd8, 'checkout', ['git checkout ']),
+        (0x0a0dd8, '-b', ['-b ']),
+        # 4th row ----------
+        (0xc70000, 'status', ['git status']),
+        (0xFFFF00,  'clone', ['git clone ']),
+        (0xc70000, 'branch', ['git branch']),
+        # Encoder button ---
+        (0x000000, '', [Keycode.ENTER]) # ENTER key
+    ]
+}
